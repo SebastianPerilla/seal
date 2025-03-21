a1f006d07422b768084a9bc183708c131e80a330
SebastianPerilla <sebastianperillaespinosa@gmail.com>
2025-03-21 16:00:02 +0100

    fix: update lenis package and configuration

--- seal-website-next/src/hooks/useSmoothScroll.ts
+++ seal-website-next/src/hooks/useSmoothScroll.ts
@@ -7,12 +7,10 @@ export const useSmoothScroll = () => {
   useEffect(() => {
     const lenis = new Lenis({
       duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      direction: 'vertical',
-      gestureDirection: 'vertical',
-      smooth: true,
-      smoothTouch: false,
+      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      wheelMultiplier: 1,
       touchMultiplier: 2,
+      infinite: false,
     });
 
     function raf(time: number) {
